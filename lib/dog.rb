819af6114e630ed87bc5cd8fe4f44d488bc0100b
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-31 17:57:18 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(dog_name)
+    @this_dogs_name = dog_name 
+  end
+  
+  def name 
+    @this_dogs_name
+  end
+  
+  def breed=(type)
+    @breed = type
+  end
+  
+  def breed 
+    @breed
+  end
+end
